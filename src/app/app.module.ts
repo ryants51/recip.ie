1bf8d9fa98b4dff397890598822e67d3eb981a67
Ryan Shrewsbury <ryanshrewsbury51@gmail.com>
2020-04-07 16:10:44 -0500

    Added some more authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeService } from './recipes/recipe.service';
 import { HttpClientModule } from '@angular/common/http';
-import { AuthComponent } from './auth/auth/auth.component';
+import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner/loading-spinner.component';
 
 @NgModule({
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { AuthService, AuthResponseData } from './auth.service';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -12,7 +14,7 @@ export class AuthComponent implements OnInit {
   isLoading = false;
   error: string = null;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -31,19 +33,22 @@ export class AuthComponent implements OnInit {
     const password = form.value.password;
     this.isLoading = true;
 
+    let authObs: Observable<AuthResponseData>;
+
     if (this.isLoginMode) {
-      // Do the login
+      authObs = this.authService.login(email, password);
     } else {
-      this.authService.signup(email, password).subscribe(resData => {
-        console.log(resData);
-        this.isLoading = false;
-      }, errorMessage => {
-        this.error = errorMessage;
-        this.isLoading = false;
-      });
+      authObs = this.authService.signup(email, password);
     }
 
-    
+    authObs.subscribe(resData => {
+      console.log(resData);
+      this.isLoading = false;
+      this.router.navigate(['./recipes']);
+    }, errorMessage => {
+      this.error = errorMessage;
+      this.isLoading = false;
+    });
 
     form.reset();
   }
