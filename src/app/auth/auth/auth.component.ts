6f8d0145b61048cd80706c54367458f1d6a68f5c
Ryan Shrewsbury <ryanshrewsbury51@gmail.com>
2020-04-07 09:19:03 -0500

    Not really sure

--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -8,8 +9,10 @@ import { NgForm } from '@angular/forms';
 })
 export class AuthComponent implements OnInit {
   isLoginMode = true;
+  isLoading = false;
+  error: string = null;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
@@ -19,7 +22,29 @@ export class AuthComponent implements OnInit {
   }
 
   onSubmit(form: NgForm) {
-    console.log(form.value);
+    if (!form.valid) {
+      return;
+    }
+
+
+    const email = form.value.email;
+    const password = form.value.password;
+    this.isLoading = true;
+
+    if (this.isLoginMode) {
+      // Do the login
+    } else {
+      this.authService.signup(email, password).subscribe(resData => {
+        console.log(resData);
+        this.isLoading = false;
+      }, errorMessage => {
+        this.error = errorMessage;
+        this.isLoading = false;
+      });
+    }
+
+    
+
     form.reset();
   }
 }
